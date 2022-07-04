0ad363a78e537bdce7e99542cbe9d836631b32a9
Lody Borgers <l.borgers@taf.nl>
2022-07-04 22:09:21 +0200

    Added support for amount, imageUrl and tags in items

---
+++ prisma/migrations/20220704195312_amount_tags/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Item" ADD COLUMN     "amount" MONEY NOT NULL DEFAULT 0.0,
+ADD COLUMN     "tags" TEXT[] DEFAULT ARRAY[]::TEXT[];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -26,17 +26,27 @@ async function seed() {
 
   await prisma.item.create({
     data: {
-      name: 'My first item',
-      description: 'Hello, world!',
+      name: 'Tim de Turtle',
+      description:
+        'Verandert de kinderkamer in een prachtige zonsondergang met vogels die door de hele kamer vliegen, terwijl je luistert naar ontspannende Afrikaanse geluiden.',
       userId: user.id,
+      amount: 100,
+      tags: ['speelgoed'],
+      imageUrl:
+        'https://www.zazu-kids.com/wp-content/uploads/2022/03/TIM-Product-shot-Projection-2400x1875-1-2048x1600.jpg',
     },
   });
 
   await prisma.item.create({
     data: {
-      name: 'My second item',
-      description: 'Hello, world!',
+      name: 'Baby Yoda romper',
+      description:
+        'Een set bestaande uit een romper, een short en een accessoire van zacht katoenen tricot met print. De romper heeft korte mouwen en een drukknoopsluiting in het kruis. De short heeft een comfortabele en mooi aansluitende pasvorm over de luier.',
       userId: user.id,
+      amount: 14.99,
+      tags: ['kleding'],
+      imageUrl:
+        'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Ff6%2Ff9%2Ff6f9b4a49d50b319ec96815406ff6ee682721743.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/fullscreen]',
     },
   });
 
